f4eab4ec75f8b6e3a8a8a8561ceca41ef0f2e74c
Alinur Satylganov <alinur.satylganov@iaau.edu.kg>
2023-01-26 17:50:28 +0600

    feat: new folder

---
+++ 23-01/script.js
@@ -0,0 +1,15 @@
+let a = 17;
+let b = 10;
+let c = a-b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+let a = Date(Date.now());
+let b = a.toString();
+console.log(b);
+
+let num = 10;
+num++;
+num++;
+alert(num);
