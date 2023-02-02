fbf335a44efa5b7563f6d299ab457f82d5716b47
Alinur01 <alinur.satylganov@iaau.edu.kg>
2023-02-02 17:23:03 +0600

    feat: added folder

---
+++ 30-01/main.js
@@ -0,0 +1,20 @@
+let Legend = {
+    a:5,
+    b:6,
+    c:10
+}
+console.log(Legend.a+Legend.b+Legend.c);
+
+let data = false;
+let number;
+if(data === true){
+    number = 3;
+} else{
+    number = 5;
+}
+console.log(number);
+
+let data = 10>9;
+let number = (data === true ? '3' : '5');
+
+console.log(number);
