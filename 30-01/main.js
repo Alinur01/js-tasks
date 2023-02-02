b3eb745e9ac1b0bfe269ede6a9a92806c8fd4cf3
Alinur01 <alinur.satylganov@iaau.edu.kg>
2023-02-02 18:41:26 +0600

    feat: added file

--- 30-01/main.js
+++ 30-01/main.js
@@ -1,20 +1,12 @@
-let Legend = {
+let legend = {
     a:5,
     b:6,
     c:10
 }
-console.log(Legend.a+Legend.b+Legend.c);
+console.log(legend.a+legend.b+legend.c);
 
-let data = false;
-let number;
-if(data === true){
-    number = 3;
-} else{
-    number = 5;
-}
-console.log(number);
 
 let data = 10>9;
-let number = (data === true ? '3' : '5');
+let number = (data === true ? 3 : 5);
 
 console.log(number);
