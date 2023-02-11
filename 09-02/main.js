48f939a0e3373e8c5acc7c5d5b331086feccaa00
Alinur01 <alinur.satylganov@iaau.edu.kg>
2023-02-11 18:30:00 +0600

    feat: upgrate code

--- 09-02/main.js
+++ 09-02/main.js
@@ -1,5 +1,7 @@
 // HomeWork
-// 1
+// 1На старте вы получаете массив. Необходимо написать функцию, которая будет
+//    возвращать массив удвоенных значений исходного массива.
+//    Пример кода:
  function DoubleValue() {
      let numbers = [1, 2, 3, 4, 5];
      return console.log(numbers.map(numbers => numbers * 2));
@@ -7,7 +9,10 @@
  let result = DoubleValue();
  console.log(result);
 
-// 2
+// 2  Напишите функцию, которая заполняет новый массив предоставленным значением
+//    и затем ее возвращает. Функция должна первым параметром принимать
+//    элемент (значение для массива), вторым параметром размер массива.
+//    Например: myFunc('a', 3)   // ['a', 'a', 'a']
  function fillArray(element, size) {
    let newArray = [];
    for (let i = 0; i < size; i++) {
@@ -19,7 +24,11 @@
  console.log(fillArray('a', 3));
 
 
-// 3
+// 3 Напишите функцию, которая разворачивает массив в обратном порядке
+//    и затем ее возвращает.  Функция наша  принимает массив любых элементов.
+//    Например:
+//    const data = [1, 2, 3];
+//    myFunc(data);  // [3, 2, 1];
  function revers(users = ['Max', 'Samir', 'Ulan']) {
      let reversed = users.reverse();
      console.log('reversed', reversed);
